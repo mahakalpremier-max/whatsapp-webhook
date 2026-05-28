e5d61b7d552c0e5496dc2c95570178aebad22ffc
mahakalpremier-max <mahakalpremier@gmail.com>
2026-05-28 09:35:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+const VERIFY_TOKEN = "mahakal123";
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post("/webhook", (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+  console.log("Webhook running");
+});
